c2cf722adb72ae98a243e8ffc27b76d2f6584873
Jain <vaibhav.jain@dxc.com>
2023-12-06 17:10:02 +0530

    Changed in SpringBootAwsExampleApplication.java

--- src/main/java/com/javatechie/springbootawsexample/SpringBootAwsExampleApplication.java
+++ src/main/java/com/javatechie/springbootawsexample/SpringBootAwsExampleApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+Vaibhav
+**/
 @RestController
 @SpringBootApplication
 public class SpringBootAwsExampleApplication {
